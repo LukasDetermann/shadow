8291a85e5363f5375060623ef2202f014b6665f2
LukasDetermann <determann@protonmail.com>
2022-09-24 17:16:00 +0200

    removed TypeKind NoType

--- shadow-api-11/src/main/java/org/determann/shadow/api/TypeKind.java
+++ shadow-api-11/src/main/java/org/determann/shadow/api/TypeKind.java
@@ -3,57 +3,55 @@ package org.determann.shadow.api;
 public enum TypeKind
 {
    //primitives
-   BOOLEAN(true, false, false, false, false),
-   BYTE(true, false, false, false, false),
-   SHORT(true, false, false, false, false),
-   INT(true, false, false, false, false),
-   LONG(true, false, false, false, false),
-   CHAR(true, false, false, false, false),
-   FLOAT(true, false, false, false, false),
-   DOUBLE(true, false, false, false, false),
+   BOOLEAN(true, false, false, false),
+   BYTE(true, false, false, false),
+   SHORT(true, false, false, false),
+   INT(true, false, false, false),
+   LONG(true, false, false, false),
+   CHAR(true, false, false, false),
+   FLOAT(true, false, false, false),
+   DOUBLE(true, false, false, false),
 
    //declared
-   CLASS(false, false, true, false, false),
-   INTERFACE(false, false, true, false, false),
-   ENUM(false, false, true, false, false),
-   ANNOTATION(false, false, true, false, false),
+   CLASS(false, true, false, false),
+   INTERFACE(false, true, false, false),
+   ENUM(false, true, false, false),
+   ANNOTATION(false, true, false, false),
 
    //executable
-   METHOD(false, false, false, true, false),
-   CONSTRUCTOR(false, false, false, true, false),
+   METHOD(false, false, true, false),
+   CONSTRUCTOR(false, false, true, false),
 
    //variables
-   ENUM_CONSTANT(false, false, false, false, true),
-   FIELD(false, false, false, false, true),
-   PARAMETER(false, false, false, false, true),
+   ENUM_CONSTANT(false, false, false, true),
+   FIELD(false, false, false, true),
+   PARAMETER(false, false, false, true),
 
    //strange stuff
-   VOID(false, true, false, false, false),
-   PACKAGE(false, true, false, false, false),
-   MODULE(false, true, false, false, false),
-   NULL(false, false, false, false, false),
+   VOID(false, false, false, false),
+   PACKAGE(false, false, false, false),
+   MODULE(false, false, false, false),
+   NULL(false, false, false, false),
 
-   ARRAY(false, false, false, false, false),
+   ARRAY(false, false, false, false),
 
-   GENERIC(false, false, false, false, false),
+   GENERIC(false, false, false, false),
 
-   UNION(false, false, false, false, false),
+   UNION(false, false, false, false),
 
-   WILDCARD(false, false, false, false, false),
+   WILDCARD(false, false, false, false),
 
-   INTERSECTION(false, false, false, false, false);
+   INTERSECTION(false, false, false, false);
 
 
    private final boolean primitive;
-   private final boolean noType;
    private final boolean declared;
    private final boolean executable;
    private final boolean variable;
 
-   TypeKind(boolean primitive, boolean noType, boolean declared, boolean executable, boolean variable)
+   TypeKind(boolean primitive, boolean declared, boolean executable, boolean variable)
    {
       this.primitive = primitive;
-      this.noType = noType;
       this.declared = declared;
       this.executable = executable;
       this.variable = variable;
@@ -62,11 +60,6 @@ public enum TypeKind
    public boolean isPrimitive()
    {
       return primitive;
-   }
-
-   public boolean isNoType()
-   {
-      return noType;
    }
 
    public boolean isDeclared()
